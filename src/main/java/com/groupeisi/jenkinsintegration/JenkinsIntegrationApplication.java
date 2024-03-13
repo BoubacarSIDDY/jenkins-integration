0974fa5fb02860ea0db80c098f6e2183c7a9deee
Boubacar Siddy Diallo <=>
2024-03-13 15:44:36 +0000

    updated welcome function

--- src/main/java/com/groupeisi/jenkinsintegration/JenkinsIntegrationApplication.java
+++ src/main/java/com/groupeisi/jenkinsintegration/JenkinsIntegrationApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsIntegrationApplication {
 	@GetMapping
 	public String welcome(){
-		return "welcome to our first jenkins integration";
+		return "welcome to our first jenkins integration :)";
 	}
 
 	public static void main(String[] args) {
